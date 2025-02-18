840ade428b7c8571c31c9c0b65213d0241328046
Valentyn Bahmatskyi <valentunbaxc@gmail.com>
2025-02-18 18:53:08 +0200

    created server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	StartServer(8795)
+}
+
+func StartServer(port int) {
+	address := fmt.Sprintf(":%d", port)
+	err := http.ListenAndServe(address, nil)
 }
